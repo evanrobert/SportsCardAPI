abb533b7392be0ea87c72eaea65f77f4a2001cd1
evan <evan.lindsay223@gmail.com>
2023-08-05 23:22:41 -0700

    adding completed postmapping for firstName, lastName

--- src/main/java/com/evanrobert/Json/API/Controller.java
+++ src/main/java/com/evanrobert/Json/API/Controller.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -14,12 +15,9 @@ public class Controller {
     UsersRepo usersRepo;
 
     @PostMapping("/add/user")
-    public ResponseEntity<String> addNewUser(Users users){
+    public ResponseEntity<String> addNewUser(@RequestBody Users users){
         usersRepo.save(users);
         String message = "User added successfully!";
         return ResponseEntity.status(HttpStatus.OK).body(message);
-
-
-
     }
 }
