a3e2357eb95aee9f3b394e1d04f856ebe3b33fe6
evan <evan.lindsay223@gmail.com>
2023-08-06 19:53:56 -0700

    fixing the if empty in Main class

--- src/main/java/com/evanrobert/Json/API/Application.java
+++ src/main/java/com/evanrobert/Json/API/Application.java
@@ -24,13 +24,15 @@ public class Application implements ApplicationRunner {
 	public void run(ApplicationArguments args) throws Exception {
 		Cards cards = new Cards();
 
-		if (cards.isEmpty()) {
+		if (usersRepo.count()==0) {
 			Cards newUser = cards.builder()
 					.player("bill russell").price(5000).rookie("no").numbered("yes")
 					.yearOfCard("1965")
 					.build();
+			Cards newCard2 = Cards.builder().player("Michael Jordan").price(50).yearOfCard("2000").numbered("No").rookie("no").build();
 
 			usersRepo.save(newUser);
+			usersRepo.save(newCard2);
 			usersRepo.flush();
 		}
 
