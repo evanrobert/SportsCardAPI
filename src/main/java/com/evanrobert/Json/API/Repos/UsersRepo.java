af3ab0a87ca27689ee7013692e6aae98a40969de
evan <evan.lindsay223@gmail.com>
2023-08-09 23:17:42 -0700

    adding comments in repo

--- src/main/java/com/evanrobert/Json/API/Repos/UsersRepo.java
+++ src/main/java/com/evanrobert/Json/API/Repos/UsersRepo.java
@@ -7,10 +7,14 @@ import java.util.List;
 
 public interface UsersRepo extends JpaRepository<Cards,Long> {
     List<Cards> findCardByRookie(boolean rookie);
+    //Finds Card by if rookie
     List<Cards> findCardByNumbered(boolean numbered);
+    //Finds Card by if numbered
     List<Cards> findCardByBrand(String brand);
+    // Finds card by Brand
 
     List<Cards> findByPlayerStartingWith(String player);
+    //Finds player starting with
 }
 
 
