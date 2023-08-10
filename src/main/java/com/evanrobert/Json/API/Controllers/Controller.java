f8d417a144571930aa1361ca729de4a389a55ef0
evan <evan.lindsay223@gmail.com>
2023-08-09 22:53:51 -0700

    adding descriptive comments on what is happening and the purpose of the code

--- src/main/java/com/evanrobert/Json/API/Controllers/Controller.java
+++ src/main/java/com/evanrobert/Json/API/Controllers/Controller.java
@@ -21,6 +21,7 @@ public class Controller {
     public List<Cards> getAllUsers() {
         return usersRepo.findAll();
     }
+    // This allows you to get all cards that have been posted
 
 
     @PostMapping("/add/user")
@@ -29,6 +30,7 @@ public class Controller {
         String message = "User added successfully!";
         return ResponseEntity.status(HttpStatus.OK).body(message);
     }
+    //This will allow you to post a card based off of all of the felds in the cards Model.
     @PatchMapping("/fix/card/{id}")
     public ResponseEntity<String> fixCard(@PathVariable Long id, @RequestBody Cards cardUpdate) {
         NotFoundException notFoundException = new NotFoundException();
@@ -69,27 +71,32 @@ public class Controller {
         String message = "Card changed successfully!";
         return ResponseEntity.status(HttpStatus.OK).body(message);
     }
+    //This searches for a card based off of an ID, when found it will allow you to modify all fields
 
     @DeleteMapping("/delete/card/{id}")
     public ResponseEntity<String>  deleteById(@PathVariable Long id){
         usersRepo.deleteById(id);
         String message = "card deleted successfully!";
         return ResponseEntity.status(HttpStatus.OK).body(message);
+        // This searches for a card based on ID and will delete the card
     }
     @GetMapping("/find/by/{rookie}")
     public ResponseEntity<List<Cards>> findCardByRookie(@PathVariable("rookie") boolean rookie) {
         List<Cards> rookieCards = usersRepo.findCardByRookie(rookie);
         return ResponseEntity.status(HttpStatus.OK).body(rookieCards);
+        // Searches for cards based off if the card is a rookie or not, this is a boolean value.
     }
     @GetMapping("/numbered/{numbered}")
     public ResponseEntity<List<Cards>> findCardByNumbered(@PathVariable("numbered") boolean numbered) {
         List<Cards> numberedCards = usersRepo.findCardByNumbered(numbered);
         return ResponseEntity.status(HttpStatus.OK).body(numberedCards);
+        // Searches for cards based off if the card is numbered or not This is a boolean value.
     }
     @GetMapping("/card/{brand}")
     public ResponseEntity<List<Cards>> findCardByBrand(@PathVariable("brand")String brand) {
         List<Cards> brandOfCards = usersRepo.findCardByBrand(brand);
         return ResponseEntity.status(HttpStatus.OK).body(brandOfCards);
+        // Searches for cards based off the brand of the card
     }
 
 
@@ -97,6 +104,7 @@ public class Controller {
     public ResponseEntity<List<Cards>> findCardByPlayer(@PathVariable("player") String player) {
         List<Cards> findByPlayerName = usersRepo.findByPlayerStartingWith(player);
         return ResponseEntity.status(HttpStatus.OK).body(findByPlayerName);
+        //Searches for a card Based off of the prefix of the player.
     }
 
 
