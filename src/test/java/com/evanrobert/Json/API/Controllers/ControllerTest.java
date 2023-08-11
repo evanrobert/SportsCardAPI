e8fbffca4edf579c26ea8404c776ee6cad40ac0a
evan <evan.lindsay223@gmail.com>
2023-08-10 22:20:57 -0700

    adding test to get all cards

--- src/test/java/com/evanrobert/Json/API/Controllers/ControllerTest.java
+++ src/test/java/com/evanrobert/Json/API/Controllers/ControllerTest.java
@@ -2,20 +2,19 @@ package com.evanrobert.Json.API.Controllers;
 
 import com.evanrobert.Json.API.Model.Cards;
 import com.evanrobert.Json.API.Repos.UsersRepo;
-import org.hibernate.service.spi.InjectService;
+
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
-import org.springframework.util.Assert;
+import org.mockito.junit.jupiter.MockitoExtension;
 
-import java.util.Arrays;
+import java.util.Collections;
 import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.Mockito.*;
-
+@ExtendWith(MockitoExtension.class)
 class ControllerTest {
     @Mock
     UsersRepo usersRepo;
@@ -24,8 +23,34 @@ class ControllerTest {
 
     @Test
     void getAllUsers() {
+        Cards fakeCard = Cards.builder()
+                    .sport("Football")
+                    .player("John Doe")
+                    .numbered(true)
+                    .price(50.0)
+                    .yearOfCard("2022")
+                    .rookie(false)
+                    .brand("Optic")
+                    .build();
 
-    }
+            List<Cards> fakeCardList = Collections.singletonList(fakeCard);
+            when(usersRepo.findAll()).thenReturn(fakeCardList);
+
+
+            List<Cards> returnedCards = controller.getAllUsers();
+
+
+            assertNotNull(returnedCards);
+            assertEquals(1, returnedCards.size());
+            assertEquals(fakeCard, returnedCards.get(0));
+
+
+            verify(usersRepo, times(1)).findAll();
+        }
+
+
+
+
 
     @Test
     void addNewUser() {
