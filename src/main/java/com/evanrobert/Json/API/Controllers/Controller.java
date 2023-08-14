257b72d302ac2ce885c6430dd22f55b5283886b6
evan <evan.lindsay223@gmail.com>
2023-08-13 20:11:30 -0700

    adding documentation for the findByYearOfCard

--- src/main/java/com/evanrobert/Json/API/Controllers/Controller.java
+++ src/main/java/com/evanrobert/Json/API/Controllers/Controller.java
@@ -25,11 +25,12 @@ public class Controller {
 
 
     @PostMapping("/add/user")
-    public ResponseEntity<String> addNewUser(@RequestBody Cards cards){
+    public ResponseEntity<String> addNewUser(@RequestBody Cards cards) {
         usersRepo.save(cards);
         String message = "User added successfully!";
         return ResponseEntity.status(HttpStatus.OK).body(message);
     }
+
     //This will allow you to post a card based off of all of the felds in the cards Model.
     @PatchMapping("/fix/card/{id}")
     public ResponseEntity<String> fixCard(@PathVariable Long id, @RequestBody Cards cardUpdate) {
@@ -54,16 +55,14 @@ public class Controller {
         if (cardUpdate.getYearOfCard() != null) {
             existingCard.setYearOfCard(cardUpdate.getYearOfCard());
         }
-        if(cardUpdate.getBrand() != null){
+        if (cardUpdate.getBrand() != null) {
             existingCard.setBrand(cardUpdate.getBrand());
         }
-        if(cardUpdate.getSport() != null){
+        if (cardUpdate.getSport() != null) {
             existingCard.setSport(cardUpdate.getSport());
         }
         existingCard.setRookie(cardUpdate.isRookie());
         existingCard.setNumbered(cardUpdate.isNumbered());
-
-
 
 
         usersRepo.save(existingCard);
@@ -74,26 +73,29 @@ public class Controller {
     //This searches for a card based off of an ID, when found it will allow you to modify all fields
 
     @DeleteMapping("/delete/card/{id}")
-    public ResponseEntity<String>  deleteById(@PathVariable Long id){
+    public ResponseEntity<String> deleteById(@PathVariable Long id) {
         usersRepo.deleteById(id);
         String message = "card deleted successfully!";
         return ResponseEntity.status(HttpStatus.OK).body(message);
         // This searches for a card based on ID and will delete the card
     }
+
     @GetMapping("/find/by/{rookie}")
     public ResponseEntity<List<Cards>> findCardByRookie(@PathVariable("rookie") boolean rookie) {
         List<Cards> rookieCards = usersRepo.findCardByRookie(rookie);
         return ResponseEntity.status(HttpStatus.OK).body(rookieCards);
         // Searches for cards based off if the card is a rookie or not, this is a boolean value.
     }
+
     @GetMapping("/numbered/{numbered}")
     public ResponseEntity<List<Cards>> findCardByNumbered(@PathVariable("numbered") boolean numbered) {
         List<Cards> numberedCards = usersRepo.findCardByNumbered(numbered);
         return ResponseEntity.status(HttpStatus.OK).body(numberedCards);
         // Searches for cards based off if the card is numbered or not This is a boolean value.
     }
+
     @GetMapping("/card/{brand}")
-    public ResponseEntity<List<Cards>> findCardByBrand(@PathVariable("brand")String brand) {
+    public ResponseEntity<List<Cards>> findCardByBrand(@PathVariable("brand") String brand) {
         List<Cards> brandOfCards = usersRepo.findCardByBrand(brand);
         return ResponseEntity.status(HttpStatus.OK).body(brandOfCards);
         // Searches for cards based off the brand of the card
@@ -110,6 +112,7 @@ public class Controller {
         }
         return ResponseEntity.status(HttpStatus.OK).body(findByPlayerName);
     }
+
     @GetMapping("/card/year/{yearOfCard}")
     public ResponseEntity<?> findCardByYearOfCard(@PathVariable("yearOfCard") String yearOfCard) {
         List<Cards> findCardByPlayersYear = usersRepo.findCardByYearOfCardStartingWith(yearOfCard);
@@ -121,9 +124,13 @@ public class Controller {
 
         return ResponseEntity.status(HttpStatus.OK).body(findCardByPlayersYear);
     }
-
-
-
-
-
 }
+    /* This GetMapping is responsible for finding a card by the year of card the user enters.
+    If there is no card year recorded, we return a not found status code, as well as a cusrtom error message.
+     If it is found we return all the cards corresponding to that year.*/
+
+
+
+
+
+
