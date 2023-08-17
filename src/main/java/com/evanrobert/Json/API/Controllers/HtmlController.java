27473c5020f58fcbfc86621ceb77e83e88c1fe78
evan <evan.lindsay223@gmail.com>
2023-08-16 22:53:37 -0700

    adding another option to search by year of card

--- src/main/java/com/evanrobert/Json/API/Controllers/HtmlController.java
+++ src/main/java/com/evanrobert/Json/API/Controllers/HtmlController.java
@@ -6,18 +6,38 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
 
+
+import java.util.Collections;
 import java.util.List;
 
 @Controller
 public class HtmlController {
     @Autowired
     CardRepo cardRepo;
-    @GetMapping("/get/cards")
-    public String getAllUsers(Model model) {
+
+    @GetMapping("/")
+    public String getAllCards(Model model) {
         List<Cards> cards = cardRepo.findAll();
+        model.addAttribute("cards", cards);
+        return "cards"; // Returning the name of the HTML template
+    }
+
+    @GetMapping("/get/cards/sport")
+    public String getCardsBySport(@RequestParam("sport") String sport, Model model) {
+        List<Cards> cards = cardRepo.findCardBySportStartingWith(sport);
+        model.addAttribute("cards", cards);
+        return "cards"; // Returning the name of the HTML template
+    }
+    @GetMapping("/get/card/yearOfCard")
+    public String getByYearOfCard(@RequestParam("yearOfCard")String yearOfCard,Model model){
+        List<Cards> cards = cardRepo.findCardByYearOfCardStartingWith(yearOfCard);
         model.addAttribute("cards", cards);
         return "cards";
     }
 
 }
+
+
