034e6d14cd337fcaeb8da184dda66d62c817d400
evan <evan.lindsay223@gmail.com>
2023-08-19 15:38:50 -0700

    adding get by numbered method

--- src/main/java/com/evanrobert/Json/API/Controllers/HtmlController.java
+++ src/main/java/com/evanrobert/Json/API/Controllers/HtmlController.java
@@ -6,12 +6,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
-
-
-import java.util.Collections;
 import java.util.List;
 
 @Controller
@@ -32,24 +28,36 @@ public class HtmlController {
         model.addAttribute("cards", cards);
         return "cards"; // Returning the name of the HTML template
     }
+
     @GetMapping("/get/card/yearOfCard")
-    public String getByYearOfCard(@RequestParam("yearOfCard")String yearOfCard,Model model){
+    public String getByYearOfCard(@RequestParam("yearOfCard") String yearOfCard, Model model) {
         List<Cards> cards = cardRepo.findCardByYearOfCardStartingWith(yearOfCard);
         model.addAttribute("cards", cards);
         return "cards";
     }
+
     @GetMapping("/get/card/by/brand")
-    public String getCardByBrand(@RequestParam("brand")String brand, Model model, RedirectAttributes redirectAttributes){
+    public String getCardByBrand(@RequestParam("brand") String brand, Model model, RedirectAttributes redirectAttributes) {
         List<Cards> cards = cardRepo.findCardByBrand(brand);
-        if(cards.isEmpty()){
-            redirectAttributes.addAttribute("error","no card by this brand");
+        if (cards.isEmpty()) {
+            redirectAttributes.addAttribute("error", "no card by this brand");
 
         }
-        model.addAttribute("cards",cards);
+        model.addAttribute("cards", cards);
         return "cards";
 
     }
 
+    @GetMapping("/get/card/by/numbered")
+    public String getCardByIsNumbered(@RequestParam("numbered") boolean numbered, Model model, RedirectAttributes redirectAttributes) {
+        List<Cards> cards = cardRepo.findCardByNumbered(numbered);
+        if (cards.isEmpty()) {
+            redirectAttributes.addAttribute("error", "no card by this brand");
+        }
+        model.addAttribute("cards", cards);
+        return "cards";
+
+    }
 }
 
 
