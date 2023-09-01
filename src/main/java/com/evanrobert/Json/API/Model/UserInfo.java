d30dfb632eac96c7f5db083b187a36cc3cfc6bb2
evan <evan.lindsay223@gmail.com>
2023-08-31 17:21:40 -0700

    adding user Entitiy

---
+++ src/main/java/com/evanrobert/Json/API/Model/UserInfo.java
@@ -0,0 +1,29 @@
+package com.evanrobert.Json.API.Model;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.validation.constraints.NotEmpty;
+
+@Entity
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class UserInfo {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @NotEmpty
+    private String name;
+    @NotEmpty
+    private String email;
+    //Need to add relationship to connect Cards and Users here...
+
+}
