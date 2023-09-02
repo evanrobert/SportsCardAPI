006eb461c0969c1572ad7ecc35fe5683b551c8c5
evan <evan.lindsay223@gmail.com>
2023-09-01 22:17:37 -0700

    adding A repo for users, and also adding fields for userLoginDetailService, need to remove service annotation at some point

--- src/main/java/com/evanrobert/Json/API/Model/UserDetailService.java
+++ src/main/java/com/evanrobert/Json/API/Model/UserDetailService.java
@@ -1,2 +1,26 @@
-package com.evanrobert.Json.API.Model;public class userDetailService {
+package com.evanrobert.Json.API.Model;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.springframework.stereotype.Service;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Service
+public class UserDetailService {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String username;
+    private String password;
 }
--- src/main/java/com/evanrobert/Json/API/Repos/UserLoginDetailsRepo.java
+++ src/main/java/com/evanrobert/Json/API/Repos/UserLoginDetailsRepo.java
@@ -3,5 +3,6 @@ package com.evanrobert.Json.API.Repos;
 import com.evanrobert.Json.API.Model.UserDetailService;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface userDetailServiceRepo extends JpaRepository <UserDetailService,Long> {
+public interface UserLoginDetailsRepo extends JpaRepository <UserDetailService,Long> {
+    UserDetailService findByUsername(String username);
 }
