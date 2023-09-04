ecfd589808adbae07a81a9891b4fca43d04685d2
evan <evan.lindsay223@gmail.com>
2023-09-03 21:13:51 -0700

    adding a repo for user information

---
+++ src/main/java/com/evanrobert/Json/API/Repos/UserInformationRepo.java
@@ -0,0 +1,9 @@
+package com.evanrobert.Json.API.Repos;
+
+import com.evanrobert.Json.API.Model.UserDetailService;
+import com.evanrobert.Json.API.Model.UserInfo;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserInformationRepo extends JpaRepository<UserInfo,Long> {
+    UserInfo findByName(String name );
+}
