bc86fbc644e04d1737e5dda226b7ad735503b360
evan <evan.lindsay223@gmail.com>
2023-09-18 23:19:01 -0700

    adding A way to access name and email

--- src/main/java/com/evanrobert/Json/API/Controllers/viewAccountDetails.java
+++ src/main/java/com/evanrobert/Json/API/Controllers/viewAccountDetails.java
@@ -6,6 +6,7 @@ import com.evanrobert.Json.API.Model.UserInfo;
 import com.evanrobert.Json.API.Repos.UserInformationRepo;
 import com.evanrobert.Json.API.Repos.UserLoginDetailsRepo;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Controller;
 
 import org.springframework.ui.Model;
@@ -29,12 +30,27 @@ public class viewAccountDetails {
         UserLoginDetailsRepo userLoginDetailsRepo;
 
         @GetMapping("/view/account")
-        private String getUserInfo() {
-            return "Account_info";
+        private String getUserInfo(UserInfo userInfo, Principal principal, Model model) {
+            String username = principal.getName();
+            UserDetailService userDetailService = userLoginDetailsRepo.findByUsername(username);
+            userInfo.setUserDetailService(userDetailService);
 
+            // Fetch the name and email from the userInfo object
+            String name = userInfo.getName();
+            String email = userInfo.getEmail();
+
+            // Add the name and email to the model for rendering in the HTML template
+            model.addAttribute("name", name);
+            model.addAttribute("email", email);
+
+            return "Account_info";
         }
+
+
+
     }
-}
+    }
+
 
 
 
