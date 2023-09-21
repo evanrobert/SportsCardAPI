fc32fea34f147f260f988291dd47f3d43559b3c1
evan <evan.lindsay223@gmail.com>
2023-09-20 22:39:40 -0700

    adding a way to access users cards

--- src/main/java/com/evanrobert/Json/API/Controllers/viewAccountDetails.java
+++ src/main/java/com/evanrobert/Json/API/Controllers/viewAccountDetails.java
@@ -1,6 +1,7 @@
 package com.evanrobert.Json.API.Controllers;
 
 
+import com.evanrobert.Json.API.Model.Cards;
 import com.evanrobert.Json.API.Model.UserDetailService;
 import com.evanrobert.Json.API.Model.UserInfo;
 import com.evanrobert.Json.API.Repos.UserInformationRepo;
@@ -14,6 +15,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 import java.security.Principal;
+import java.util.List;
 import java.util.Optional;
 
 @Controller
@@ -34,8 +36,10 @@ public class viewAccountDetails {
             String username = principal.getName();
             UserDetailService userDetailService = userLoginDetailsRepo.findByUsername(username);
             UserInfo userInfo = userDetailService.getUserInfo(); // Retrieve UserInfo from UserDetailService
+           List<Cards> cards = userInfo.getCards();
 
             // Fetch the name and email from the userInfo object and add them to the model
+            model.addAttribute("cards",cards);
             model.addAttribute("userInfo", userInfo);
             model.addAttribute("name", userInfo.getName());
             model.addAttribute("email", userInfo.getEmail());
